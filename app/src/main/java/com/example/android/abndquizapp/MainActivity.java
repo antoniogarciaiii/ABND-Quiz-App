0f345fd30693a1afbc6092a123e1411ecd5d66c7
Antonio Garcia <antoniogarciaiii@gmail.com>
2018-06-02 14:16:23 -0500

    began code for reset button. cleaned up comments.

--- app/src/main/java/com/example/android/abndquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/abndquizapp/MainActivity.java
@@ -20,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     int score = 0;
 
-    //each question adds 20 to the score--as in 20%, 'cause basic math.
-    // finals, because they don't change
+    //each question adds 20 to the score--as in 20%
+    // using finals for the answer, because they are static
     final int q1_Answer = R.id.q1_radiobutton_d;
     /* question 2 will be handled by its own method, because it's a
        checkbox w/2 answers that needs to work off a conditional statement */
@@ -36,59 +36,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // gets student's name, checks answers, displays score
-    public void checkAnswers(View view){
-
+    // sets score to zero, checks answers, displays score
+    public void checkAnswers(View view) {
+        score = 0;
         checkQuestion1();
         checkQuestion2();
         checkQuestion3();
         checkQuestion4();
         checkQuestion5();
 
+        // find way to use string resource for score preamble. alternatively, display score only
         String toastMessage = "you got " + score + "%";
         Toast grades = Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_LONG);
         grades.show();
     }
 
 
-
-    //checks question 1
-    private /*boolean*/ void checkQuestion1() {
-        //so, when i kept hitting the 'get a grade' button, it kept adding.
-        //this is kind of a poor man's solution, but i'll find the proper solution later.
-        score = 0;
+    //check question 1
+    private void checkQuestion1() {
+        //if the pressed radio button is the same as the answer, add 20 pts.
         RadioGroup rg = (RadioGroup) findViewById(R.id.q1_group);
 
-        if( rg.getCheckedRadioButtonId() == q1_Answer ) {
+        if (rg.getCheckedRadioButtonId() == q1_Answer) {
             score += 20;
-//            return true;
         }
-
-//        return false; // it's false, but we're not going to do anything with it. we're just kinda gonna move on.
     }
 
-    // method to check question 2. the idea is to gather their states, one by one, then check them.
+    // check question 2. the idea is to gather their states, one by one, then check them.
     private void checkQuestion2() {
         CheckBox q2CA = (CheckBox) findViewById(R.id.q2_a);
         CheckBox q2CB = (CheckBox) findViewById(R.id.q2_b);
         CheckBox q2CC = (CheckBox) findViewById(R.id.q2_c);
         CheckBox q2CD = (CheckBox) findViewById(R.id.q2_d);
-        // if, and only if: A is checked, AND B is checked, AND C is not checked, AND C is not checked, return true.
-        // if true is returned, add 20 to score.
+        // IF A is checked, & B is checked, & C is not checked, & D is not checked, add 20 pts to score.
         if (q2CA.isChecked() && q2CB.isChecked() && !q2CC.isChecked() && !q2CD.isChecked()) {
             score += 20;
         }
     }
 
-    // method to check question 3.
+    // check question 3.
     public void checkQuestion3() {
         //extract value from q3 answer field, convert to string
         EditText q3UserAnswerET = findViewById(R.id.q3_user_answer);
         String q3UserAnswer = q3UserAnswerET.getText().toString();
         //convert to lowercase, to make it more user friendly for this question.
-        //question 5 needs to be correct with capitalization, but this one is fine.
         q3UserAnswer = q3UserAnswer.toLowerCase();
-        if (q3UserAnswer.equals(q3_Answer)){
+        if (q3UserAnswer.equals(q3_Answer)) {
             score += 20;
         }
     }
@@ -97,8 +90,8 @@ public class MainActivity extends AppCompatActivity {
     //check question 4
     private void checkQuestion4() {
         RadioGroup rg = (RadioGroup) findViewById(R.id.q4_group);
-
-        if( rg.getCheckedRadioButtonId() == q4_Answer ) {
+        // if correct radio button is pressed, add 20 pts to score
+        if (rg.getCheckedRadioButtonId() == q4_Answer) {
             score += 20;
         }
 
@@ -106,12 +99,22 @@ public class MainActivity extends AppCompatActivity {
 
     // method to check question 5.
     private void checkQuestion5() {
-        //same logic as question 3.
+        // extracts the user answer, converts into a string.
+        // then, the checks to see if it's the same as the answer.
+        // if same, add 20 pts to score
         EditText q5UserAnswerET = findViewById(R.id.q5_user_answer);
         String q5UserAnswer = q5UserAnswerET.getText().toString();
         if (q5UserAnswer.equals(q5_Answer)) {
             score += 20;
         }
+    }
+
+    // reset score, uncheck boxes, unpress radio buttons, empty input boxes
+    private void resetAnswers() {
+        score = 0;
+        // reset q1
+
+
     }
 
 }
